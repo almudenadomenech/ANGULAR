22cf09a24f81931b42aafcb4d3627f6173150e22
almudenadomenech <violeta_33@hotmail.es>
2024-02-02 14:30:46 +0100

    angular crear componentes

--- angular-002-componentes/src/app/app.component.ts
+++ angular-002-componentes/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { PedidosComponent } from "./pedidos/pedidos.component";
+import { ProductosComponent } from "./productos/productos.component";
+import { ClientesComponent } from "./clientes/clientes.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [PedidosComponent, ProductosComponent, ClientesComponent]
 })
 export class AppComponent {
   title = 'angular-002-componentes';
