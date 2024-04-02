3507ac83801f36abc79806a7e8724b765ca51de7
almudenadomenech <violeta_33@hotmail.es>
2024-04-02 10:56:30 +0200

    añadido rating y comentarios en booking-detail

--- FULLSTACK/fullstack-001/frontend/src/app/book-detail/book-detail.component.ts
+++ FULLSTACK/fullstack-001/frontend/src/app/book-detail/book-detail.component.ts
@@ -5,12 +5,14 @@ import { Author } from '../interfaces/author.model';
 import { Book } from '../interfaces/book.model';
 import { Reservation } from '../interfaces/reservation.model';
 import { Rating } from '../interfaces/rating.model';
-import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbRatingConfig, NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
+import { DatePipe } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-book-detail',
   standalone: true,
-  imports: [HttpClientModule, RouterLink, NgbRatingModule],
-
+  imports: [HttpClientModule, RouterLink, NgbRatingModule, DatePipe, ReactiveFormsModule],
+  //providers: [NgbRatingModule],
   templateUrl: './book-detail.component.html',
   styleUrl: './book-detail.component.css'
 })
@@ -19,9 +21,14 @@ export class BookDetailComponent implements OnInit{
   book: Book | undefined;
   reservations: Reservation[] = [];
   ratings: Rating[] = [];
+  // formulario para crear nuevos comentarios
+  ratingForm = new FormGroup({
+    score: new FormControl(0),
+  comment: new FormControl('')
+  });
 
   constructor(private httpClient: HttpClient,
-    private activatedRoute: ActivatedRoute) {}
+    private activatedRoute: ActivatedRoute,) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
